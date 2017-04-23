47e26005dfe06e922339baf821c087b176894568
Insa <m.kram@live.at>
2017-04-23 21:28:16 +0200

    Update Readme

--- readme.md
+++ readme.md
@@ -15,7 +15,12 @@ pr0verter is a Web Application for manipulating Video Data with FFmpeg.
 ## Install instructions
 Download this Repository via Clone or Zip Download <br>
 Run `composer install`, `cp .env.example .env`, `php artisan key:generate`<br>
-Edit .env to your needs <br>
-Run `php artisan migrate`
 
-Have fun!
+Edit .env to your needs <br>
+Run `php artisan migrate` <br>
+If your queue driver is database, run `php artisan queue:work --queue=convert,default` otherwise your Videos won't get converted.
+
+
+###Important notes for .env: <br>
+You NEED an <a href="https://github.com/settings/tokens">Github Access Token</a> for the changelog to work <br>
+If you want to run the converter in background set `QUEUE_DRIVER=sync` to `QUEUE_DRIVER=database`
---
+++ resources/assets/js/stepbystepform.js
@@ -0,0 +1,27 @@
+var $firstButton = $(".first"),
+    $secondButton = $(".second"),
+    $input = $("input"),
+    $name = $(".name"),
+    $more = $(".more"),
+    $yourname = $(".yourname"),
+    $reset = $(".reset"),
+    $ctr = $(".container-steps");
+
+$firstButton.on("click", function(e){
+    $(this).text("Saving...").delay(900).queue(function(){
+        $ctr.addClass("center slider-two-active").removeClass("full slider-one-active");
+    });
+    e.preventDefault();
+});
+
+$secondButton.on("click", function(e){
+    $(this).text("Saving...").delay(900).queue(function(){
+        $ctr.addClass("full slider-three-active").removeClass("center slider-two-active slider-one-active");
+        $name = $name.val();
+        if($name == "") {
+            $yourname.html("Anonymous!");
+        }
+        else { $yourname.html($name+"!"); }
+    });
+    e.preventDefault();
+});
