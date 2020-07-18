8c56203147d086eeb1dc81c816db0c1e535bc868
name <email@email.email>
2020-07-18 19:34:03 +0200

    Add schmuser.png

--- resources/views/error/errorVid.blade.php
+++ resources/views/error/errorVid.blade.php
@@ -5,6 +5,7 @@
         <div class="jumbotron">
             <div class="row">
                 <h1>Fehler beim Konvertieren, der pr0verter kann dein Video nicht richtig Verarbeiten ¯\_(ツ)_/¯</h1>
+                <image width="30%" src="{{ asset('images/schmuser.png') }}"></image>
             </div>
         </div>
     </div>
